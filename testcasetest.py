c331e8085fc40e4a97ff9056b1a2aaef37cb6bcc
Tom <tvandort1@gmail.com>
2018-04-09 13:52:37 -0400

    Initial Commit

---
+++ testcasetest.py
@@ -0,0 +1,10 @@
+from testcase import TestCase
+from wasrun import WasRun
+
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test= WasRun("testMethod")
+        assert(not test.wasRun)
+        test.run()
+        assert(test.wasRun)
+TestCaseTest("testRunning").run()
