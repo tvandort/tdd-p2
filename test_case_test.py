caf10f536263219cdf8c82b9b05c205dee72d52e
tvand <tvandort1@gmail.com>
2018-04-10 12:53:13 -0400

    Refactor parts to setup.

--- test_case_test.py
+++ test_case_test.py
@@ -6,43 +6,40 @@ from test_suite import TestSuite
 
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.result= TestResult()
+
     def testTemplateMethod(self):
         test= WasRun("testMethod")
-        result= TestResult()
-        test.run(result)
+        test.run(self.result)
         assert("setUp testMethod tearDown " == test.log)
 
     def testResult(self):
         test= WasRun("testMethod")
-        result= TestResult()
-        test.run(result)
-        assert("1 run, 0 failed" == result.summary())
+        test.run(self.result)
+        assert("1 run, 0 failed" == self.result.summary())
 
     def testFailedResult(self):
         test= WasRun("testBrokenMethod")
-        result= TestResult()
-        test.run(result)
-        assert("1 run, 1 failed" == result.summary())
+        test.run(self.result)
+        assert("1 run, 1 failed" == self.result.summary())
 
     def testFailedResultFormatting(self):
-        result= TestResult()
-        result.testStarted()
-        result.testFailed()
-        assert("1 run, 1 failed" == result.summary())
+        self.result.testStarted()
+        self.result.testFailed()
+        assert("1 run, 1 failed" == self.result.summary())
 
     def setupFailed(self):
         test= BrokenSetup("testMethod")
-        result= TestResult()
-        test.run(result)
-        assert("1 run, 1 failed" == result.summary())
+        test.run(self.result)
+        assert("1 run, 1 failed" == self.result.summary())
 
     def testSuite(self):
         suite= TestSuite()
         suite.add(WasRun("testMethod"))
         suite.add(WasRun("testBrokenMethod"))
-        result= TestResult()
-        suite.run(result)
-        assert("2 run, 1 failed" == result.summary())
+        suite.run(self.result)
+        assert("2 run, 1 failed" == self.result.summary())
 
 # tests = [f for _, f in TestCaseTest.__dict__.items() if callable(f)]
 
@@ -59,6 +56,7 @@ suite.add(TestCaseTest("testFailedResultFormatting"))
 suite.add(TestCaseTest("testFailedResult"))
 suite.add(TestCaseTest("testSuite"))
 suite.add(TestCaseTest("setupFailed"))
+
 result= TestResult()
 suite.run(result)
 print(result.summary())
